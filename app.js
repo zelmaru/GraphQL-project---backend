3490594a50bdf5edffc2d09567f23aef5de8f8a4
zelmaru <zelenkovait@gmail.com>
2021-05-12 21:11:37 +0200

    add mongoose and restructure files

--- app.js
+++ app.js
@@ -1,83 +1,38 @@
 require("dotenv").config();
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
 const schema = require("./graphql/schema/schema");
 const rootValue = require("./graphql/resolvers/rootValue");
-const { buildSchema } = require("graphql");
-const bcrypt = require("bcrypt");
 
 const app = express();
-
-const users = [];
-const saltRounds = 12;
-
-const transformUser = (user) => {
-	return { ...user, password: null };
-};
 
 app.use(
 	"/graphql",
 	graphqlHTTP({
-		schema: buildSchema(`
-    type User {
-      username: String!
-      password: String
-    }
-    
-    type RootQuery {
-      users: [User!]!
-      loginUser(username: String!, password: String!): User!
-    }
-
-    input UserInput {
-      username: String!
-      password: String!
-    }
-    
-    type RootMutation {
-      createUser(userInput: UserInput): User
-    }
-    
-    schema {
-      query: RootQuery
-      mutation: RootMutation
-    }
-    `),
-		rootValue: {
-			users: () => {
-				return users.map((user) => transformUser(user));
-			},
-			createUser: async (args) => {
-				console.log(args);
-				const { username, password } = args.userInput;
-				const user = {
-					username,
-					password: await bcrypt.hash(password, saltRounds),
-				};
-				users.push(user);
-				return transformUser(user);
-			},
-			loginUser: async ({ username, password }) => {
-				let user = users.find((user) => user.username === username);
-				if (!user) {
-					throw new Error("User not found", 404);
-				}
-
-				const passwordOK = await bcrypt.compare(password, user.password);
-
-				if (!passwordOK) {
-					throw new Error("Wrong credentials", 401);
-				}
-
-				return transformUser(user);
-			},
-		},
+		schema,
+		rootValue,
 		graphiql: true,
 	})
 );
 
 const PORT = 3001 || process.env.PORT;
 
-app.listen(PORT, () => {
-	console.log(`Server started on port ${PORT}`);
-});
+const connectDb = async () => {
+	try {
+		await mongoose.connect(
+			`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.h9zgq.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+			}
+		);
+		app.listen(PORT, () => {
+			console.log(`Server started on port ${PORT}`);
+		});
+	} catch (err) {
+		throw new Error("Could not connect to a database", err);
+	}
+};
+
+connectDb();
--- graphql/resolvers/rootValue.js
+++ graphql/resolvers/rootValue.js
@@ -1,36 +1,72 @@
 const bcrypt = require("bcrypt");
+const User = require("../../models/user");
 
 const users = [];
 const saltRounds = 12;
 
 const transformUser = (user) => {
-	return { ...user, password: null };
+	return { ...user, password: null }; // use .toJSON(),
 };
 
 module.exports = {
-	users: () => {
+	users: async () => {
+		let users;
+		try {
+			users = await User.find({}, "-password"); // hide password hash
+		} catch (err) {
+			throw new Error("Could not fetch users", 500);
+		}
+
+		if (!users) {
+			throw new Error("No users found", 404);
+		}
+
 		return users.map((user) => transformUser(user));
 	},
 	createUser: async (args) => {
 		console.log(args);
 		const { username, password } = args.userInput;
-		const user = {
+
+		let existingUser;
+
+		try {
+			existingUser = await User.findOne({ username: username });
+		} catch (err) {
+			throw new Error("Registration failed", 500);
+		}
+
+		if (existingUser) {
+			throw new Error("This email is already in use", 422);
+		}
+
+		const user = new User({
 			username,
 			password: await bcrypt.hash(password, saltRounds),
-		};
-		users.push(user);
+		});
+
+		try {
+			await user.save();
+		} catch (err) {
+			throw new Error("Registration failed", 500);
+		}
 		return transformUser(user);
 	},
 	loginUser: async ({ username, password }) => {
-		let user = users.find((user) => user.username === username);
+		let user;
+		try {
+			user = await User.findOne({ username });
+		} catch (err) {
+			throw new Error("Login failed", 500);
+		}
+
 		if (!user) {
-			throw new Error("User not found", 404);
+			throw new Error("User with the given username does not exist", 404);
 		}
 
 		const passwordOK = await bcrypt.compare(password, user.password);
 
 		if (!passwordOK) {
-			throw new Error("Wrong credentials", 401);
+			throw new Error("Check your password", 401);
 		}
 
 		return transformUser(user);
